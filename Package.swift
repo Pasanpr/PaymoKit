de493bd795c14657ce7b3a963ae54627836cbad6
Pasan Premaratne <pasan@premaratne.me>
2018-06-15 13:48:53 -0400

    Using v4.2 of SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,14 +1,30 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "PaymoKit",
-	products: [
-		.library(name: "PaymoKit", targets: ["PaymoKit"]),
-	],
-	targets: [
-		.target(name: "PaymoKit"),
-		.testTarget(name: "PaymoTests")
-	]
+    name: "PaymoKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PaymoKit",
+            targets: ["PaymoKit"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "PaymoKit",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "PaymoKitTests",
+            dependencies: ["PaymoKit"],
+            path: "Tests"),
+    ]
 )
